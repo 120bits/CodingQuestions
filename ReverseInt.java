847f3472c547ac7be207d9929f8adbd653b7276b
Prat Mishra <hax0755@gmail.com>
2021-04-15 21:35:26 -0600

    Reverse an Integer

---
+++ ReverseInt.java
@@ -0,0 +1,8 @@
+    public static int reverse(int num) {
+        int sum = 0;   
+        while (num != 0) {
+            sum = (sum * 10) + (num % 10);
+            num = num / 10;
+        }
+        return sum;
+    }
